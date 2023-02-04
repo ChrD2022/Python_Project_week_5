1017d75a966abcc6e55ded7fedddfab7d26c94c8
ChrD2022 <103274391+ChrD2022@users.noreply.github.com>
2023-02-04 12:03:59 +0000

    product model

---
+++ models/product.py
@@ -0,0 +1,8 @@
+class product:
+
+    def __init__(self, name, description, in_stock, buy_cost, sell_cost):
+        self.name = name
+        self.description = description
+        self.in_stock = in_stock
+        self.buy_cose = buy_cost
+        self.sell_cost = sell_cost
