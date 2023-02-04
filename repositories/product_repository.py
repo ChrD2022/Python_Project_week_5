35f521c561e4e85e988f153479fec942f8607db6
ChrD2022 <103274391+ChrD2022@users.noreply.github.com>
2023-02-04 14:28:55 +0000

    products select_all

--- repositories/product_repository.py
+++ repositories/product_repository.py
@@ -1,3 +1,35 @@
 from db.run_sql import run_sql
 
 from models.product import Product
+from models.manufacturer import Manufacturer
+import repositories.manufacturer_repository as manufacturer_repository
+
+def save(product):
+    sql = "INSERT INTO products (product_name, product_description, in_stock, buy_cost, sell_cost, manufacturer_id) VALUES (%s, %s, %s, %s, %s, %s) RETURNING *"
+    values = [product.product_name, product.product_description, product.in_stock, product.buy_cost, product.sell_cost, product.manufacturer.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    product.id = id
+    return product
+
+def select_all():
+    products = []
+
+    sql = "SELECT * FROM products"
+    results = run_sql(sql)
+
+    for row in results:
+        manufacturer = manufacturer_repository.select(row['manufacturer_id'])
+        product = Product(
+            row['product_name'],
+            row['product_description'],
+            row['in_stock'],
+            row['buy_cost'],
+            row['sell_cost'],
+            manufacturer
+            )
+        products.append(product)
+    return products
+
+
+
