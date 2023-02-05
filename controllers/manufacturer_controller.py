a079523d93635efc9a7dfee55daba43574ec06e3
ChrD2022 <103274391+ChrD2022@users.noreply.github.com>
2023-02-05 14:52:16 +0000

    manufacturer edits

--- controllers/manufacturer_controller.py
+++ controllers/manufacturer_controller.py
@@ -22,8 +22,24 @@ def new_manufacturer():
     manufacturers = manufacturer_repository.select_all()
     return render_template("/manufacturers/new.html", manufacturers = manufacturers)
 
+@manufacturer_blueprint.route("/manufacturers", methods=['POST'])
+def create_manufacturer():
+    manufacturer_name = request.form['manufacturer_name']
+    manufacturer_location = request.form['manufacturer_location']
+    manufacturer = Manufacturer(manufacturer_name, manufacturer_location)
+    manufacturer_repository.save(manufacturer)
+    return redirect('/manufacturers')
 
 @manufacturer_blueprint.route("/manufacturers/<id>/delete", methods=['POST'])
 def delete_manufacturer(id):
     manufacturer_repository.delete(id)
     return redirect('/manufacturers')
+
+edit
+@manufacturer_blueprint.route("/manufacturers/<id>/edit")
+def edit_manufacturer(id):
+    manufacturer = manufacturer_repository.select(id)
+    return render_template('/manufacturers/edit.html', manufacturer = manufacturer)
+
+update
+
