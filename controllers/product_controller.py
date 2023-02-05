3e1d79e3d2455ef8709d218599464adfa9788a5b
ChrD2022 <103274391+ChrD2022@users.noreply.github.com>
2023-02-05 11:46:49 +0000

    new product

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -20,7 +20,18 @@ def show_product(id):
 
 @product_blueprint.route("/products/new")
 def new_product():
-    products = product_repository.select_all()
-    return render_template("products/new.html", products = products)
+    manufacturers = manufacturer_repository.select_all()
+    return render_template("products/new.html", all_manufacturers = manufacturers)
 
-
+@product_blueprint.route("/products", methods=['POST'])
+def create_new_product():
+    product_name = request.form['product_name']
+    product_description = request.form['product_description']
+    in_stock = request.form['in_stock']
+    buy_cost = request.form['buy_cost']
+    sell_cost = request.form['sell_cost']
+    manufacturer_id = request.form['manufacturer_id']
+    manufacturer = manufacturer_repository.select(manufacturer_id)
+    product = Product(product_name, product_description, in_stock, buy_cost, sell_cost, manufacturer)
+    product_repository.save(product)
+    return redirect('/products')
