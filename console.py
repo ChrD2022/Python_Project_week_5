7df16fdf1fafa28aecf531e0d52dd9dd690fbb52
ChrD2022 <103274391+ChrD2022@users.noreply.github.com>
2023-02-08 11:05:56 +0000

    tamplatw tweek

--- console.py
+++ console.py
@@ -14,11 +14,11 @@ manufacturer_2 = Manufacturer("Sienar Fleet sys.", "Lianna")
 manufacturer_repository.save(manufacturer_2)
 
 product_1 = Product("X-Wing", "The perfect starfighter for your rebelling needs.",
-50, 150000, 175000, manufacturer_1)
+20, 150000, 175000, manufacturer_1)
 product_repository.save(product_1)
 product_2 = Product("TIE Fighter", "An Imperial classic.", 100, 60000, 80000, manufacturer_2)
 product_repository.save(product_2)
-product_3 = Product("TIE Interceptor", "The Imperial gentleman's choice.", 60, 75000, 90000, manufacturer_2)
+product_3 = Product("TIE Interceptor", "The Imperial gentleman's choice.", 0, 75000, 90000, manufacturer_2)
 product_repository.save(product_3)
 
 
